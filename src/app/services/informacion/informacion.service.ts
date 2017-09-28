12c3201105b7f758c109c8ed007bcc357412802e
Irving Aguilar <iragbe79@gmail.com>
2017-09-28 09:38:09 -0500

    load team

--- src/app/services/informacion/informacion.service.ts
+++ src/app/services/informacion/informacion.service.ts
@@ -5,15 +5,31 @@ import { Http} from "@angular/http";
 export class InformacionService {
 
   info:any = {};
-  cargada:boolean = false;
+  team:any[] = [];
+  loadInfo:boolean = false;
+  loadTeam:boolean = false;
 
   constructor(public http:Http) {
+    this.carga_Info();
+    this.carga_About();
+  }
+
+  carga_Info(){
     this.http.get("assets/data/info.pagina.json")
              .subscribe( data => {
                console.log(data.json());
                this.info = data.json();
-               this.cargada = true;
-             })
+               this.loadInfo = true;
+             });
+  }
+
+  carga_About(){
+    this.http.get("https://myapp-9022e.firebaseio.com/equipo.json")
+             .subscribe( data => {
+               console.log(data.json());
+               this.team = data.json();
+               this.loadTeam = true;
+             });
   }
 
 }
