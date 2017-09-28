dec6327abc76eaf4986e04df09172639c6c6d246
Irving Aguilar <iragbe79@gmail.com>
2017-09-28 15:59:19 -0500

    version 1. fixes0

--- src/app/components/portafolio/portafolio.component.ts
+++ src/app/components/portafolio/portafolio.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductosService} from "../../services/productos/productos.service";
+
 @Component({
   selector: 'app-portafolio',
   templateUrl: './portafolio.component.html',
 })
 export class PortafolioComponent {
 
-  constructor(private _ps:ProductosService) { }
-    
+  constructor(public _ps:ProductosService) { }
+
 }
